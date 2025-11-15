3b8267259e5dff2ad40a20e995cb3caa14e638f0
Germán Pablo Gonzalez <germangonzalezdev@gmail.com>
2025-11-15 16:18:49 -0300

    Commit inicial, header component

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Header } from './components/Header'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <Header />
+      <h1 className='bg-red-600 text-white'>Hola Simpsons!</h1>
+    </>
+  )
+}
+
+export default App
